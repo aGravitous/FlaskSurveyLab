dca72a67e8ff08477d8d94ee1ae6949f2fed7adf
Ethan Shen <ethanshen.wu@gmail.com>
2019-02-14 13:00:34 -0800

    landing page added

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ app.secret_key = "Vive La France"
 
 
 @app.route('/')
-def 
+def landing_screen():
+    '''display screen with title, instructions, and start button'''
+    title_of_survey = surveys.satisfaction_survey.title
+    survey_instructions = surveys.satisfaction_survey.instructions
+    return render_template('landing_page.html', title_of_survey=title_of_survey, survey_instructions=survey_instructions)
+
