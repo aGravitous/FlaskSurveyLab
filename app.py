4066f82aa710d0b27c8871581c4aac8359bfc2e3
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-14 15:32:30 -0800

    Added details to question page route

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ def landing_screen():
     '''display screen with title, instructions, and start button'''
     title_of_survey = surveys.satisfaction_survey.title
     survey_instructions = surveys.satisfaction_survey.instructions
+    session['answers'] = []
     return render_template('landing_page.html', title_of_survey=title_of_survey, survey_instructions=survey_instructions)
 
 
-@app.route('/questions/0')
-def testing():
-    ''''''
+@app.route('/questions/<question_index>', methods=["POST"])
+def testing(question_index):
+    '''Present first question, POST response on submit.'''
+    q_at_index = surveys.satisfaction_survey.questions[question_index]
+    question_text = q_at_index.question
+    choices = q_at_index.choices
+
+    answers = session['answers']
+    answers.append[f'{choice}']
+    session['answers'] = answers
     return render_template('base.html')
