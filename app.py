0243d40cc32c1910f5125d4a5a63d454af786b0d
Ethan Shen <ethanshen.wu@gmail.com>
2019-02-14 16:35:57 -0800

    basic functioning of survey

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, redirect, flash
 import surveys
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.secret_key = "Vive La France"
 
+debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def landing_screen():
@@ -14,14 +16,32 @@ def landing_screen():
     return render_template('landing_page.html', title_of_survey=title_of_survey, survey_instructions=survey_instructions)
 
 
-@app.route('/questions/<question_index>', methods=["POST"])
-def testing(question_index):
+@app.route('/questions/<int:question_index>')
+def survey_start(question_index):
     '''Present first question, POST response on submit.'''
     q_at_index = surveys.satisfaction_survey.questions[question_index]
     question_text = q_at_index.question
-    choices = q_at_index.choices
+    choice_list = q_at_index.choices
+    
+    
+    return render_template('survey_page.html', 
+                            question_text=question_text, 
+                            choice_list=choice_list, 
+                            new_question_i=question_index)
+
+
+@app.route('/questions/<int:question_index>', methods=["POST"])
+def survey_continue(question_index):
+    '''Present first question, POST response on submit.'''
+    
+    answer = request.form["choice"]
 
     answers = session['answers']
-    answers.append[f'{choice}']
+    answers.append(answer)
     session['answers'] = answers
-    return render_template('base.html')
+    new_question_i = question_index + 1
+
+    if new_question_i == len(surveys.satisfaction_survey.questions):
+        flash('''Thank you for participating in this survey. 
+                        We appreciate your time and effort.''')
+        return redirect(f'/questions/{new_question_i}')
